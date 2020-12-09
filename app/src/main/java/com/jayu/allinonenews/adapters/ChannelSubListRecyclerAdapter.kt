094d5360e2817afe9e7b2c658f06fcb6065e2513
Jay Parmar <developerjayu@gmail.com>
2020-12-09 20:19:32 +0530

    09-12

--- app/src/main/java/com/jayu/allinonenews/adapters/ChannelSubListRecyclerAdapter.kt
+++ app/src/main/java/com/jayu/allinonenews/adapters/ChannelSubListRecyclerAdapter.kt
@@ -4,48 +4,87 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.*
 import com.jayu.allinonenews.R
 import com.jayu.allinonenews.fragments.NewsFragment
 import com.jayu.allinonenews.models.ChannelSubList
 import com.jayu.allinonenews.utils.toast
 
-class ChannelSubListRecyclerAdapter(val context : Context, val channelSubList : ArrayList<ChannelSubList>) : RecyclerView.Adapter<ChannelSubListRecyclerAdapter.viewHolder>() {
+
+class ChannelSubListRecyclerAdapter(
+    val context: Context,
+    val channelSubList: ArrayList<ChannelSubList>
+) : RecyclerView.Adapter<ChannelSubListRecyclerAdapter.viewHolder>() {
 
     private lateinit var mInterstitialAd: InterstitialAd
+//    private lateinit var mAdView : AdView
 
-    class viewHolder(view : View) : RecyclerView.ViewHolder(view){
-        val parentId : TextView = view.findViewById(R.id.channelParentId)
-        val id : TextView = view.findViewById(R.id.channelSubListId)
-        val url : TextView = view.findViewById(R.id.channelUrl)
-        val name : TextView = view.findViewById(R.id.txtChannelSubListName)
+    class viewHolder(view: View, viewType: Int) : RecyclerView.ViewHolder(view){
+        val parentId : TextView? = view.findViewById(R.id.channelParentId)
+        val id : TextView? = view.findViewById(R.id.channelSubListId)
+        val url : TextView? = view.findViewById(R.id.channelUrl)
+        val name : TextView? = view.findViewById(R.id.txtChannelSubListName)
+        val mAdView : AdView? = view.findViewById(R.id.mainAds)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position %5 ==0){
+            0
+        }
+        else{
+            1
+        }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): viewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_channel_sub_list_single_row,parent,false)
 
-        return viewHolder(view)
+        val view : View
+
+        if (viewType == 0){
+
+            view = LayoutInflater.from(parent.context).inflate(
+                R.layout.ads,
+                parent,
+                false
+            )
+        }
+
+        else{
+            view = LayoutInflater.from(parent.context).inflate(
+                R.layout.item_channel_sub_list_single_row,
+                parent,
+                false
+            )
+        }
+
+        return viewHolder(view,viewType)
     }
 
     override fun onBindViewHolder(holder: viewHolder, position: Int) {
+
+        val adView = AdView(context)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = context.getString(R.string.aionBanner)
+
+        MobileAds.initialize(context){}
+        holder.mAdView?.loadAd(AdRequest.Builder().build())
 
         mInterstitialAd = InterstitialAd(context)
         mInterstitialAd.adUnitId = context.getString(R.string.aionInterstital)
         mInterstitialAd.loadAd(AdRequest.Builder().build())
 
         val number = channelSubList[position]
-        holder.parentId.text = number.parentId.toString()
-        holder.id.text = number.id.toString()
-        holder.url.text = number.url
-        holder.name.text = number.name
+        holder.parentId?.text = number.parentId.toString()
+        holder.id?.text = number.id.toString()
+        holder.url?.text = number.url
+        holder.name?.text = number.name
 
         holder.itemView.setOnClickListener{
 
@@ -54,7 +93,7 @@ class ChannelSubListRecyclerAdapter(val context : Context, val channelSubList : 
             }
 
             (it.context as AppCompatActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment,NewsFragment())
+                .replace(R.id.nav_host_fragment, NewsFragment())
                 .addToBackStack(null)
                 .commit()
         }
